f7ed1e7d74a1520c571711771b89b40c786746d3
emlie <mi-ya@live.no>
2018-04-06 15:21:03 +0200

    add recipes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,15 @@
 /*
-if space between window and top is greater than 850: make nav change background and font-color
+if space between window and top is greater than 800: make nav change background and font-color
 [from jsfiddle:
 http://jsfiddle.net/onigetoc/1e8k9q8b/]
-
-make house icon change color (change icon src)
-[from https://codepen.io/fabuchao/pen/xwbRaa]
 */
 
 
 var myNav = $("nav");
 
-
 $(window).on("scroll", function() {
   "use strict";
-  if ($(window).scrollTop() >= 850) {
+  if ($(window).scrollTop() >= 800) {
     myNav.addClass("scroll");
   } else {
     myNav.removeClass("scroll");
