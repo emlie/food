58f20f3d22f2aeb131d8ff137b052bdefab28629
emlie <mi-ya@live.no>
2018-04-06 14:38:07 +0200

    add scroll

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,63 +9,13 @@ make house icon change color (change icon src)
 
 
 var myNav = $("nav");
-var myNavMobile = $(".navMobile");
-var myHouse = $("nav ul li a img");
 
 
 $(window).on("scroll", function() {
   "use strict";
   if ($(window).scrollTop() >= 850) {
     myNav.addClass("scroll");
-    myHouse.attr("src", "../ikoner/home-gray.svg");
   } else {
     myNav.removeClass("scroll");
-    myHouse.attr("src", "../ikoner/home.svg");
   }
-});
-
-$(window).on("scroll", function() {
-  "use strict";
-  if ($(window).scrollTop() >= 550) {
-    myNavMobile.addClass("scroll");
-    myHouse.attr("src", "../ikoner/home-gray.svg");
-  } else {
-    myNavMobile.removeClass("scroll");
-    myHouse.attr("src", "../ikoner/home.svg");
-  }
-});
-
-
-
-
-
-
-
-
-/*
-hamburger menu for mobile nav:
-http://www.qdigitalstudio.com/library/responsive-toggle-menu
-
-if the function finds the next .navigation is visible,
-toggle its visibility
-*/
-
-(function($) {
-  $.fn.collapsable = function(options) {
-    // iterate and reformat each matched element
-    return this.each(function() {
-      // cache this:
-      var obj = $(this);
-      var tree = obj.next('.navigation');
-      obj.click(function() {
-        if (obj.is(':visible')) {
-          tree.toggle();
-        }
-      });
-    });
-  };
-})(jQuery);
-
-$(document).ready(function() {
-    $('.slide-trigger').collapsable();
 });
